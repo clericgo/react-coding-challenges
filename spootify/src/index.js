5b50fef3f473ed9ace01fd2fc2139b24d1b0869e
clericgo <clericgo@gmail.com>
2021-11-10 17:56:06 -0600

    Without Changes

--- spootify/src/index.js
+++ spootify/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import CoreLayout from './common/layouts/CoreLayout';
 import './styles/_main.scss';
+import { SpotifyApiContext } from 'react-spotify-api';
 
 ReactDOM.render(
   <React.StrictMode>
     <CoreLayout>
-      <Routes />
+      <SpotifyApiContext.Provider value={token}>
+        <Routes />
+      </SpotifyApiContext.Provider>
     </CoreLayout>
   </React.StrictMode>,
   document.getElementById('root')
